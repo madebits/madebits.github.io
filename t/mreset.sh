986360ab4e1895ce8c9cf03b8d70b011954f5c3a
madebits <madebits@no-email.com>
2015-10-03 12:00:00 +0000

    commit

--- t/mreset.sh
+++ t/mreset.sh
@@ -1,10 +1,10 @@
 #! /bin/bash -x
 
 if [[ ! $1 ]]; then exit 1; fi
-REPO=$1
-URL="https://github.com/madebits/${REPO}.git"
-git clone $URL
-cd "$REPO"
+repo=$1
+URL="https://github.com/madebits/${repo}.git"
+git clone "$url"
+cd "$repo"
 rm -rf .git
 git init
 git config user.name madebits
@@ -12,6 +12,6 @@ git config user.email madebits@no-email.com
 git add .
 CDATE="Sat, 3 Oct 2015 12:00:00 +0000"
 GIT_AUTHOR_DATE="${CDATE}" GIT_COMMITTER_DATE="${CDATE}" git commit --date="${CDATE}" -m "commit"
-git remote add origin $URL
+git remote add origin "$url"
 git push -u --force origin master
 cd ..
