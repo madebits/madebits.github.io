907f3b89c1f2026afaa07a46ff8202c8939cf56f
madebits <madebits@no-email.com>
2015-10-03 12:00:00 +0000

    commit

--- mg.sh
+++ mg.sh
@@ -1,69 +1,114 @@
 #!/bin/bash
 
-#I: folder pass resize
+INPUTDIR=.
+RESIZE=100%
+SUFFIX=".dx"
+OUTDIR="./mb-gallery"
+THUMBSIZE=100
 
-if [[ ! -d "$1" || -z "$2" ]]; then
-	echo "Usage: $0 folder pass"
+if [[ $# -eq 0 ]] ; then
+    echo 'Usage: -i inputDir -p password [-r resize] [-s suffix] [-o outputDir] [-t thumbSize]'
+    exit 1
+fi
+
+while [[ $# > 1 ]]
+do
+key="$1"
+
+case $key in
+    -i|--input)
+    INPUTDIR="$2"
+    shift
+    ;;
+    -p|--password)
+    PASS="$2"
+    shift
+    ;;
+    -r|--resize)
+    RESIZE="$2"
+    shift
+    ;;
+    -s|--suffix)
+    SUFFIX="$2"
+    shift
+    ;;
+    -o|--output)
+    OUTDIR="$2/gallery"
+    shift
+    ;;
+    -t|--thumbsize)
+    THUMBSIZE="$2"
+    shift
+    ;;
+
+    #--default)
+    #DEFAULT=YES
+    #;;
+    *)
+    echo "Unknown option $1"
+    exit 1
+    ;;
+esac
+shift
+done
+
+if [[ -z "$INPUTDIR" ]]; then
+	echo "-i required"
+	exit 1
+fi
+if [[ -z "$PASS" ]]; then
+	echo "-p required"
 	exit 1
 fi
 
-folder="$1"
-pass="$2"
-resize=100
-if [[ -n "$3" ]]; then
-	resize="$3"
-fi
-
-suffix=".dx"
-
-if [[ -n "$4" ]]; then
-	suffix="$4"
-fi
-
-urlPrefix="@@@"
-outDir="mb-gallery"
-outFile="${outDir}/index.md"
+URLPREFIX="@@@"
+outFile="${OUTDIR}/index.md"
+rm -rf "${OUTDIR}"
 count=0
-rm -rf "${outDir}"
-total=$(find "${folder}" -maxdepth 1 -type f -iname "*.jpg" | wc -l)
+total=$(find "${INPUTDIR}" -maxdepth 1 -type f -iname "*.jpg" | wc -l)
 if [[ $total -eq 0 ]]; then
 	exit 0
 fi
-mkdir "${outDir}"
+mkdir -p "${OUTDIR}"
 
 echo -e "#<i class='fa fa-th'></i> Gallery\n\n" >> "${outFile}"
 
-find "${folder}" -maxdepth 1 -type f -iname "*.jpg" | sort | while read filePath; do
+find "${INPUTDIR}" -maxdepth 1 -type f -iname "*.jpg" | sort | while read filePath; do
 	count=$((count+1))	
 	echo "${count} / ${total} : ${filePath}"
-	fileData=$(convert -resize ${resize}% "$filePath" jpeg:- | base64 -w 0)
-	fileDataThumb=$(convert -resize 100x100 "$filePath" jpeg:- | base64 -w 0)
+	fileData=$(convert -resize ${RESIZE} "$filePath" jpeg:- | base64 -w 0)
+	fileDataThumb=$(convert -resize ${THUMBSIZE}x${THUMBSIZE} "$filePath" jpeg:- | base64 -w 0)
 	
-	echo -e "[![@nosave@@inline@](data:image/jpeg;base64,${fileDataThumb})](${urlPrefix}${count}${suffix}) " >> "${outFile}"
+	echo -e "[![@nosave@@inline@](data:image/jpeg;base64,${fileDataThumb})](${URLPREFIX}${count}${SUFFIX}) " >> "${outFile}"
 
-	links="[<i class='fa fa-th'></i>](${urlPrefix}index${suffix})"
+	links="[<i class='fa fa-th'></i>](${URLPREFIX}index${SUFFIX})"
 	prevCount=$((count-1))
 	nextCount=$((count+1))
 	if [[ $prevCount -ge 1 ]]; then
-		links="[<i class='fa fa-chevron-circle-left'></i>](${urlPrefix}${prevCount}${suffix}) ${links}"
+		links="[<i class='fa fa-chevron-circle-left'></i>](${URLPREFIX}${prevCount}${SUFFIX}) ${links}"
 	fi
 	if [[ $nextCount -le $total ]]; then
-		links="${links} [<i class='fa fa-chevron-circle-right'></i>](${urlPrefix}${nextCount}${suffix})"
+		links="${links} [<i class='fa fa-chevron-circle-right'></i>](${URLPREFIX}${nextCount}${SUFFIX})"
 	fi
 
-	echo -e "${count} / ${total} ${links}\n\n" >> "${outDir}/${count}.md"
+	#echo -e "${count} / ${total} ${links}\n\n" >> "${OUTDIR}/${count}.md"
+	echo -e "<br>" >> "${OUTDIR}/${count}.md"
 	if [[ $nextCount -le $total ]]; then
-		echo -e "[![@nosave@@responsive@](data:image/jpeg;base64,${fileData})](${urlPrefix}${nextCount}${suffix})\n\n" >> "${outDir}/${count}.md"
+		echo -e "[![@nosave@@responsive@](data:image/jpeg;base64,${fileData})](${URLPREFIX}${nextCount}${SUFFIX})\n\n" >> "${OUTDIR}/${count}.md"
 	else
-		echo -e "![@nosave@](data:image/jpeg;base64,${fileData})\n\n" >> "${outDir}/${count}.md"
+		echo -e "[![@nosave@@responsive@](data:image/jpeg;base64,${fileData})](${URLPREFIX}1${SUFFIX})\n\n" >> "${OUTDIR}/${count}.md"
 	fi
-	echo -e "${count} / ${total} ${links}\n\n" >> "${outDir}/${count}.md"
+	echo -e "${count} / ${total} ${links}\n\n" >> "${OUTDIR}/${count}.md"
 
-	$(./menc.js "${outDir}/${count}.md" "${pass}" > "${outDir}/${count}${suffix}")
-	$(rm "${outDir}/${count}.md")
+	$(./menc.js "${OUTDIR}/${count}.md" "${PASS}" > "${OUTDIR}/${count}${SUFFIX}")
+	$(rm "${OUTDIR}/${count}.md")
 done
 
-echo -e "\n\n**#${total}**\n" >> "${outFile}"
+echo -e "\n\n<i class='fa fa-picture-o'></i> #${total}\n" >> "${outFile}"
 
-$(./menc.js "${outFile}" "${pass}" > "${outDir}/index${suffix}")
+$(./menc.js "${outFile}" "${PASS}" > "${OUTDIR}/index${SUFFIX}")
 $(rm "${outFile}")
+
+biggestFile=$(find ${OUTDIR} -maxdepth 1 -printf '%s %p\n' | sort -nr | head -1 | cut -d ' ' -f 2)
+echo "Biggest file (max 11M):"
+du -h "${biggestFile}"
