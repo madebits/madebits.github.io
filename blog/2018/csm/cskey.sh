856e95e87a4ca4bbe03038f867e4b47d7e874ee0
madebits <madebits@no-email.com>
2017-10-03 12:00:00 +0000

    commit

--- blog/2018/csm/cskey.sh
+++ blog/2018/csm/cskey.sh
@@ -357,6 +357,7 @@ function getSecret()
         if [ "${cskUseURandom}" = "1" ]; then
             logError "# secret: generating new (-su)"
             # https://security.stackexchange.com/questions/3936/is-a-rand-from-dev-urandom-secure-for-a-login-key
+            # https://www.2uo.de/myths-about-urandom/
             # dmesg | grep random:
             secret=$(head -c 512 /dev/urandom | base64 -w 0)
         else
