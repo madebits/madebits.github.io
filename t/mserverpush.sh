a90745d12e3a45b8a652f499fe0bf9d6a88bb4c6
madebits <madebits@no-email.com>
2017-10-03 12:00:00 +0000

    commit

--- t/mserverpush.sh
+++ t/mserverpush.sh
@@ -3,6 +3,6 @@
 git config user.name madebits
 git config user.email madebits@no-email.com
 git add .
-CDATE="Sat, 3 Oct 2015 12:00:00 +0000"
+CDATE="Tue, 3 Oct 2017 12:00:00 +0000"
 GIT_AUTHOR_DATE="${CDATE}" GIT_COMMITTER_DATE="${CDATE}" git commit --date="${CDATE}" -am "commit"
 git push -u origin master
